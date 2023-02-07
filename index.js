8c7ffe08fa374ae0a1c7b647a2b54dc62ecbc7de
JackieCisneros <krieger.jacqueline@gmail.com>
2023-02-07 15:16:14 -0700

    finished the first bonus with help from Alex, but why does my cart additions not stay when i toggle between items in the sidebar?

--- index.js
+++ index.js
@@ -28,28 +28,46 @@ const price = document.querySelector('#dish-price');
             menu.appendChild(spanItem)
             spanItem.addEventListener('click', () => {
                 populateMenuItems(item)
-            } )
+            })
         })
-    } //console.log(menu)
+    } 
 
     function populateMenuItems(food){
        image.src = food.image;
        name1.innerText = food.name;
        description.innerText = food.description;
        price.innerText = food.price;
+       bag.innerText = food.number_in_bag
+       idVariable = food.id
+       
+       console.log(idVariable)
     }
-    
+
+let idVariable;    
 const cartForm = document.querySelector('#cart-form');
 const bag = document.querySelector('#number-in-cart');
-// const cart = document.querySelector('#cart-amount');
 
-cartForm.addEventListener('submit', (event) => {
+
+cartForm.addEventListener('submit', (e) => {
+    updatingCart(e)
+})
+
+function updatingCart(event){
     event.preventDefault();
     const input = parseInt(event.target[0].value)
     bag.innerText = input + parseInt(bag.innerText);
-    console.log(event.target[0].value);
-})
+    console.log(event);
 
-    
+    fetch(`http://localhost:3000/menu/${idVariable}`,{
+        method: 'PATCH',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({number_in_bag: bag.innerText})
+    })
+    .then((resp) => resp.json())
+    .then((data)=>{
+        (data)
+    })
+}  
 
-        
